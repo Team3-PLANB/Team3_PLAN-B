d3f335ba878420924ac8bf1474dd8dea6668b26c
DESKTOP-6DJEB1H\MorsSola1105 <anna.gang46@gmail.com>
2017-06-14 21:00:11 +0900

    MessageDao 수정

--- PLANB_JEJU/src/com/planb_jeju/dao/MessageDao.java
+++ PLANB_JEJU/src/com/planb_jeju/dao/MessageDao.java
@@ -3,7 +3,7 @@ package com.planb_jeju.dao;
 import java.sql.SQLException;
 import java.util.List;
 
-import com.planb_jeju.dto.member;
+import com.planb_jeju.dto.Message;
 
 //CRUD
 public interface MessageDao {
@@ -12,13 +12,13 @@ public interface MessageDao {
 	public int getCount() throws ClassNotFoundException, SQLException;
 	
 	//쪽지 추가 (보내기)
-	public int insert(member member) throws ClassNotFoundException, SQLException;
+	public int insert(Member member) throws ClassNotFoundException, SQLException;
 	
 	//쪽지 상세보기 (메시지 형식)
-	public member getMember(String username) throws ClassNotFoundException, SQLException;
+	public Member getMember(String username) throws ClassNotFoundException, SQLException;
 		
 	//읽음여부 업데이트
-	public int update(member member) throws ClassNotFoundException, SQLException;
+	public int update(Member member) throws ClassNotFoundException, SQLException;
 	
 	//쪽지 삭제
 	public int delete(String username) throws ClassNotFoundException, SQLException;
