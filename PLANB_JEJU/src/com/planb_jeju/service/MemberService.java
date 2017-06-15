18e867b2e20c538e71e19e3ed9ff13e762693202
LAPTOP-3B23JCFE\dahye <dahye9666@gmail.com>
2017-06-15 14:38:00 +0900

    이메일 중간

--- PLANB_JEJU/src/com/planb_jeju/service/MemberService.java
+++ PLANB_JEJU/src/com/planb_jeju/service/MemberService.java
@@ -1,5 +1,6 @@
 package com.planb_jeju.service;
 
+import java.util.Properties;
 import java.util.Random;
 
 import org.apache.ibatis.session.SqlSession;
@@ -9,23 +10,5 @@ public class MemberService {
 	@Autowired
 	private SqlSession sqlsession;
 	
-	// 7자리 영문+숫자 랜덤코드 만들기
-	public String RandomNum(){
-		Random rnd = new Random();
-		StringBuffer buf = new StringBuffer();
-
-		for (int i = 0; i < 7; i++) {
-			if (rnd.nextBoolean()) {
-				buf.append((char) ((int) (rnd.nextInt(26)) + 97));
-			} else {
-				buf.append((rnd.nextInt(10)));
-			}
-		}
-		return buf.toString();
-	}
 	
-	// 메일 보내기
-	public void sendEmail(String email, String authNum){
-		
-	}
 }
