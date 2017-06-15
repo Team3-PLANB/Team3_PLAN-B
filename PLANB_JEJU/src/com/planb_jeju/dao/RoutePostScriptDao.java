4c6d723d282b845fc5f045b609b56b42197265a5
KOSTA-HP\KOSTA <hdb0408@gmail.com>
2017-06-15 21:54:47 +0900

    routePostScript mapper/dao 수정할고얌~~

---
+++ PLANB_JEJU/src/com/planb_jeju/dao/RoutePostScriptDao.java
@@ -0,0 +1,25 @@
+package com.planb_jeju.dao;
+
+import java.sql.SQLException;
+
+import com.planb_jeju.dto.Member;
+import com.planb_jeju.dto.RoutePostscript;
+
+//CRUD
+public interface RoutePostScriptDao {
+	//전체 수
+	public int getCount() throws ClassNotFoundException, SQLException;
+	
+	//게시물 입력
+	public int insert(Member member) throws ClassNotFoundException, SQLException;
+	
+	//게시물 상세
+	public Member getRoutePost(int route_code, String username) throws ClassNotFoundException, SQLException;
+		
+	//게시물 수정
+	public int update(RoutePostscript route) throws ClassNotFoundException, SQLException;
+	
+	//게시물 삭제
+	public int delete(int route_code, String username) throws ClassNotFoundException, SQLException;
+
+}
