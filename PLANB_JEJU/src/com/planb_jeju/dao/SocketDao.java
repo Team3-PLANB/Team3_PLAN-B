fea9174b9d1a99eeacd77d158ced8f61d1a226c5
DESKTOP-6DJEB1H\MorsSola1105 <anna.gang46@gmail.com>
2017-07-03 23:30:14 +0900

    뱃지 비동기 처리 완료

--- PLANB_JEJU/src/com/planb_jeju/dao/SocketDao.java
+++ PLANB_JEJU/src/com/planb_jeju/dao/SocketDao.java
@@ -1,8 +1,12 @@
 package com.planb_jeju.dao;
 
+import com.planb_jeju.dto.Message;
+
 public interface SocketDao {
 
     int selectMessageUnreadCount(String username);
+    
+    Integer updateMessageUnreadCount(Message message);
 
     boolean isMember(String username);
 }
